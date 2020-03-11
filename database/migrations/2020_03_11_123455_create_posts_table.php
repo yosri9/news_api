d8e821ff02885b5ad7ee2c28e887f01f2c161a09
yosri abdedayem <abdedayemyosri@gmail.com>
2020-03-11 14:34:35 +0100

    add migration

---
+++ database/migrations/2020_03_11_123455_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+
+            $table->dateTime(('date_written'));
+
+            $table->string(('featured_image'))->nullable();
+            $table->integer('votes_up')->nullable();
+            $table->integer('vote_down')->nullable();
+
+            //relationships
+            $table->integer('user_id');
+            $table->integer('category_id');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
