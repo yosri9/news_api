20805cd614dad6b765e0c35e87ea839020078ffe
yosri abdedayem <abdedayemyosri@gmail.com>
2020-03-13 11:04:26 +0100

    Api end point

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Resources\UsersResource;
-use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,11 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('users', function () {
-    $users = User::all();
-    return new UsersResource($users);
+/**
+ * @User Related
+ */
+Route::get('authors', 'Api\UserController@index');
+Route::get('authors/{id}', 'Api\UserController@show');
 
-});
+
+//End User Related
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
