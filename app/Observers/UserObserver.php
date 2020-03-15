2ed65ec4c6042b14d3b3395d37a972ff01c7054e
yosri abdedayem <abdedayemyosri@gmail.com>
2020-03-15 18:10:34 +0100

    add api_token to user , add user observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\User;
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        $user->api_token = bin2hex(openssl_random_pseudo_bytes(30));
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
